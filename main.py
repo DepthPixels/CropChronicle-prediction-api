9c7846cf5145746ffe84b19c7dc292b1f73ef507
DepthPixels <depthclawclashedor@gmail.com>
2024-10-17 05:12:24 +0530

    Middleware Change

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from model_interface import irrigation_data, predict_yield
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/predictions/")
 def predictions(latitude: float, longitude: float, crop_type: str):
